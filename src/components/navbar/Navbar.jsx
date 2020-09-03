baaa33ae68206738e5ccc36e366ab15cf0bc71b9
David Lajos Mihalovits <dmihalovits@gmail.com>
2020-09-03 20:30:38 +0700

    new

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -20,7 +20,9 @@ import {
     getNotifications,
     readNotification,
 } from "../../redux/actions/actions";
-const socket = require("socket.io-client")("http://localhost:5000");
+const socket = require("socket.io-client")(
+    "https://zoocial-backend.herokuapp.com"
+);
 
 const Navbar = (props) => {
     const [search, setSearch] = useState("");
